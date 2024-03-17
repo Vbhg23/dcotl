0d51a89142e243c687f976314b4706c7d8652f87
f1lded <128279948+Vbhg23@users.noreply.github.com>
2024-03-17 12:54:30 +0600

    n

---
+++ src/main/kotlin/notnggyu/dcotl/DcotlApplication.kt
@@ -0,0 +1,11 @@
+package notnggyu.dcotl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DcotlApplication
+
+fun main(args: Array<String>) {
+	runApplication<DcotlApplication>(*args)
+}
---
+++ src/test/kotlin/notnggyu/dcotl/DcotlApplicationTests.kt
@@ -0,0 +1,13 @@
+package notnggyu.dcotl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DcotlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
